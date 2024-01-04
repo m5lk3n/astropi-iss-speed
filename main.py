6653de1f02f34ca01ddf50d323060c43303b4579
Michael Klein <michael@lttl.dev>
2024-01-04 19:50:09 +0100

    add exif to image

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from math import radians, cos, sin, asin, sqrt
 from exif import Image
 from logzero import logger
 from picamera import PiCamera
+from orbit import ISS
 
 
 # https://stackoverflow.com/questions/4913349/haversine-formula-in-python-bearing-and-distance-between-two-gps-points
@@ -75,8 +76,8 @@ def get_location_as_lat_lon(image):
     return lat, lon
 
 
-def get_image_meta_data(i):
-    with open(f"{IMAGE_PATH}{i}.jpg", "rb") as image_file:  # open read, binary
+def get_image_meta_data(iteration):
+    with open(f"{IMAGE_PATH}{iteration}.jpg", "rb") as image_file:  # open read, binary
         image = Image(image_file)
         time_in_seconds = get_time_in_seconds_since_epoch(image)
         lat, lon = get_location_as_lat_lon(image)
@@ -85,28 +86,58 @@ def get_image_meta_data(i):
     return time_in_seconds, lat, lon
 
 
-def take_picture(cam, i):
+# source: https://projects.raspberrypi.org/en/projects/mission-space-lab-creator-guide/2
+def convert(angle):
+    # Convert a `skyfield` Angle to an Exif-appropriate
+    # representation (positive rationals)
+    # e.g. 98° 34' 58.7 to "98/1,34/1,587/10"
+
+    # Return a tuple containing a Boolean and the converted angle,
+    # with the Boolean indicating if the angle is negative
+
+    sign, degrees, minutes, seconds = angle.signed_dms()
+    exif_angle = f"{degrees:.0f}/1,{minutes:.0f}/1,{seconds*10:.0f}/10"
+    return sign < 0, exif_angle
+
+
+# source: https://projects.raspberrypi.org/en/projects/mission-space-lab-creator-guide/2
+def take_picture(iss, camera, iteration):
     if IMAGE_PATH == "tests/":
         return
 
-    logger.info(f"taking picture {IMAGE_PATH}{i}.jpg")
-    cam.capture(f"{IMAGE_PATH}{i}.jpg")
-    # TODO: exif
+    # Use `camera` to capture an `image` file with lat/long Exif data
+    point = iss.coordinates()
+
+    # Convert the latitude and longitude to Exif-appropriate
+    # representations
+    south, exif_latitude = convert(point.latitude)
+    west, exif_longitude = convert(point.longitude)
+
+    # Set the Exif tags specifying the current location
+    camera.exif_tags["GPS.GPSLatitude"] = exif_latitude
+    camera.exif_tags["GPS.GPSLatitudeRef"] = "S" if south else "N"
+    camera.exif_tags["GPS.GPSLongitude"] = exif_longitude
+    camera.exif_tags["GPS.GPSLongitudeRef"] = "W" if west else "E"
+
+    logger.info(f"taking picture {IMAGE_PATH}{iteration}.jpg")
+
+    # Capture the image
+    camera.capture(f"{IMAGE_PATH}{iteration}.jpg")
 
 
-def calc_speed_from_pictures(i):
-    if i <= 0:
+def calc_speed_from_pictures(iteration):
+    if iteration <= 0:
         logger.info("nothing to compare")
         return (
             -1
         )  # we define a negative speed as an "invalid" value to indicate that we couldn't calculate
 
-    logger.info(f"comparing pictures {i}.jpg and {i - 1}.jpg")
+    logger.info(f"comparing pictures {iteration}.jpg and {iteration - 1}.jpg")
 
-    time_in_seconds_cur, lat_cur, lon_cur = get_image_meta_data(i)
+    time_in_seconds_cur, lat_cur, lon_cur = get_image_meta_data(iteration)
     logger.info(f"current: {time_in_seconds_cur}, {lat_cur}, {lon_cur}")
 
-    time_in_seconds_prev, lat_prev, lon_prev = get_image_meta_data(i - 1)
+    time_in_seconds_prev, lat_prev, lon_prev = get_image_meta_data(iteration - 1)
     logger.info(f"prev: {time_in_seconds_prev}, {lat_prev}, {lon_prev}")
 
     time_in_seconds = time_in_seconds_cur - time_in_seconds_prev
@@ -114,10 +145,10 @@ def calc_speed_from_pictures(i):
     logger.info(f"distance between images in km: {distance_in_km}")
     logger.info(f"time between images in s: {time_in_seconds}")
 
-    speed_in_kmps = distance_in_km / time_in_seconds
-    logger.info(f"speed in km/s: {speed_in_kmps}")
+    speed = distance_in_km / time_in_seconds
+    logger.info(f"speed in km/s: {speed}")
 
-    return abs(speed_in_kmps)  # absolute value just in case
+    return abs(speed)  # absolute value just in case
 
 
 ## https://projects.raspberrypi.org/en/projects/mission-space-lab-creator-guide/3
@@ -132,13 +163,12 @@ i = 0  # iteration
 cam = PiCamera()
 cam.resolution = (4056, 3040)
 
-
 while now_time < start_time + timedelta(minutes=MAX_DURATION_IN_MINS):
     if i >= MAX_ITERATIONS:
         break  # exit while
 
     logger.info(f"iteration {i} started")
-    take_picture(cam, i)
+    take_picture(ISS(), cam, i)
     speed_in_kmps = calc_speed_from_pictures(i)
     logger.info(f"iteration {i} ended in {speed_in_kmps} km/s")
 
@@ -158,15 +188,15 @@ if i > 1:  # we needed at least 2 iterations for a comparison
 
 avg_speed_in_kmps_formatted = "{:.5f}".format(avg_speed_in_kmps)
 result_file = "result.txt"
-with open(result_file, "w") as file:
+with open(result_file, "w", encoding="utf-8") as file:
     file.write(avg_speed_in_kmps_formatted)
 file.close()
 
 logger.info(f"result written to {result_file}")
 
-logger.info(f"Calculated travel speed of the ISS: {avg_speed_in_kmps_formatted} km/s")
+logger.info(f"calculated travel speed of the ISS: {avg_speed_in_kmps_formatted} km/s")
 # https://projects.raspberrypi.org/en/projects/astropi-iss-speed/7
-logger.info("Actual travel speed of the ISS: 7.66 km/s")
+logger.info("actual travel speed of the ISS: 7.66 km/s")
 
 
 logger.info("ended")
